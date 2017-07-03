21266e4c2ce6a6a7503e9ba4acc4024bb7106e4a
Eric Hagman <emhagman@gmail.com>
2017-07-03 16:47:24 -0400

    Temp add DB name

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e
 
 cat << EOF > /etc/pgbouncer/pgbconf.ini
 [databases]
-* = host=${PG_ADDR} port=${PG_PORT} user=${PG_USER} auth_user=${PG_USER} password=${PG_PASS}
+d813ar4djnrejq = host=${PG_ADDR} port=${PG_PORT} dbname=d813ar4djnrejq user=${PG_USER} auth_user=${PG_USER} password=${PG_PASS}
 
 [pgbouncer]
 ;logfile = /var/log/postgresql
