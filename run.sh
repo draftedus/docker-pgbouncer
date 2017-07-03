0ddd53a11ae98c3d5dd217836f678ea368874473
Eric Hagman <emhagman@gmail.com>
2017-07-03 16:41:08 -0400

    Add password to auth_file

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e
 
 cat << EOF > /etc/pgbouncer/pgbconf.ini
 [databases]
-* = host=${PG_ADDR} port=${PG_PORT} user=${PG_USER} password=${PG_PASS}
+* = host=${PG_ADDR} port=${PG_PORT} user=${PG_USER} auth_user=${PG_USER}
 
 [pgbouncer]
 ;logfile = /var/log/postgresql
@@ -28,6 +28,7 @@ EOF
 
 
 echo '"'"${PGBOUNCER_USER}"'" "'"${PGBOUNCER_PASS}"'"'  > /etc/pgbouncer/userlist.txt
+echo '"'"${PG_USER}"'" "'"${PG_PASS}"'"'  > /etc/pgbouncer/userlist.txt
 
 chown -R app:app /etc/pgbouncer
 chmod 640 /etc/pgbouncer/userlist.txt
