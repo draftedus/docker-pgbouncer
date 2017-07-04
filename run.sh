b91336c251ebc1e87cbad8506d0232de73ba1b73
Eric Hagman <emhagman@gmail.com>
2017-07-04 01:10:43 -0400

    Use buildpack code from Heroku

--- run.sh
+++ run.sh
@@ -2,33 +2,75 @@
 
 set -e
 
-cat << EOF > /etc/pgbouncer/pgbconf.ini
-[databases]
-* = host=${PG_ADDR} port=${PG_PORT} user=${PG_USER} auth_user=${PG_USER}
+POSTGRES_URLS=${PGBOUNCER_URLS:-DATABASE_URL}
+POOL_MODE=${PGBOUNCER_POOL_MODE:-transaction}
+SERVER_RESET_QUERY=${PGBOUNCER_SERVER_RESET_QUERY}
+n=1
 
+# if the SERVER_RESET_QUERY and pool mode is session, pgbouncer recommends DISCARD ALL be the default
+# http://pgbouncer.projects.pgfoundry.org/doc/faq.html#_what_should_my_server_reset_query_be
+if [ -z "${SERVER_RESET_QUERY}" ] &&  [ "$POOL_MODE" == "session" ]; then
+  SERVER_RESET_QUERY="DISCARD ALL;"
+fi
+
+cat >> /etc/pgbouncer/pgbconf.ini << EOFEOF
 [pgbouncer]
-logfile = /var/log/postgresql/logging.txt
-pidfile = /var/run/postgresql/pgbouncer.pid
 listen_addr = 0.0.0.0
 listen_port = 6432
 unix_socket_dir = /var/run/postgresql
-auth_type = trust
+auth_type = md5
 auth_file = /etc/pgbouncer/userlist.txt
-pool_mode = session
-server_reset_query = DISCARD ALL
-max_client_conn = ${PG_MAX_CLIENT_CONN}
-default_pool_size = ${PG_DEFAULT_POOL_SIZE}
-ignore_startup_parameters = extra_float_digits
-server_idle_timeout = ${PG_SERVER_IDLE_TIMEOUT}
-log_connections = 0
-log_disconnections = 0
-stats_period = 3600
+
+; When server connection is released back to pool:
+;   session      - after client disconnects
+;   transaction  - after transaction finishes
+;   statement    - after statement finishes
+pool_mode = ${POOL_MODE}
+server_reset_query = ${SERVER_RESET_QUERY}
+max_client_conn = ${PGBOUNCER_MAX_CLIENT_CONN:-100}
+default_pool_size = ${PGBOUNCER_DEFAULT_POOL_SIZE:-1}
+min_pool_size = ${PGBOUNCER_MIN_POOL_SIZE:-0}
+reserve_pool_size = ${PGBOUNCER_RESERVE_POOL_SIZE:-1}
+reserve_pool_timeout = ${PGBOUNCER_RESERVE_POOL_TIMEOUT:-5.0}
+server_lifetime = ${PGBOUNCER_SERVER_LIFETIME:-3600}
+server_idle_timeout = ${PGBOUNCER_SERVER_IDLE_TIMEOUT:-600}
+log_connections = ${PGBOUNCER_LOG_CONNECTIONS:-1}
+log_disconnections = ${PGBOUNCER_LOG_DISCONNECTIONS:-1}
+log_pooler_errors = ${PGBOUNCER_LOG_POOLER_ERRORS:-1}
+stats_period = ${PGBOUNCER_STATS_PERIOD:-60}
 server_tls_sslmode=allow
 verbose=2
-EOF
+[databases]
+EOFEOF
 
+for POSTGRES_URL in $POSTGRES_URLS
+do
+  POSTGRES_URL_VALUE=$POSTGRES_URL
+  IFS=':' read DB_USER DB_PASS DB_HOST DB_PORT DB_NAME <<< $(echo $POSTGRES_URL_VALUE | perl -lne 'print "$1:$2:$3:$4:$5" if /^postgres(?:ql)?:\/\/([^:]*):([^@]*)@(.*?):(.*?)\/(.*?)$/')
 
-echo '"'${PG_USER}'" "'${PG_PASS}'"'  > /etc/pgbouncer/userlist.txt
+  DB_MD5_PASS="md5"`echo -n ${DB_PASS}${DB_USER} | md5sum | awk '{print $1}'`
+
+  CLIENT_DB_NAME="db${n}"
+
+  echo "Setting ${POSTGRES_URL}_PGBOUNCER config var"
+
+  if [ "$PGBOUNCER_PREPARED_STATEMENTS" == "false" ]
+  then
+    export ${POSTGRES_URL}_PGBOUNCER=postgres://$DB_USER:$DB_PASS@127.0.0.1:6000/$CLIENT_DB_NAME?prepared_statements=false
+  else
+    export ${POSTGRES_URL}_PGBOUNCER=postgres://$DB_USER:$DB_PASS@127.0.0.1:6000/$CLIENT_DB_NAME
+  fi
+
+  cat >> /etc/pgbouncer/userlist.txt << EOFEOF
+"$DB_USER" "$DB_MD5_PASS"
+EOFEOF
+
+  cat >> /etc/pgbouncer/pgbconf.ini << EOFEOF
+$CLIENT_DB_NAME= dbname=$DB_NAME port=610${n}
+EOFEOF
+
+  let "n += 1"
+done
 
 chown -R app:app /etc/pgbouncer
 mkdir -p /var/log/postgresql
